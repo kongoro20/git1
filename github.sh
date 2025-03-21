83b29faf9436491867fc60aa35f06205b597e847
kongoro20 <kongo6411@gmail.com>
2025-03-21 10:23:43 +0100

    Add files via upload

---
+++ github.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Activate the virtual environment
+python script.py
+python codespace.py
+python boss.py
+python newtab.py
+python boss.py
+
+sleep 2
+while wmctrl -l | grep -i "Mozilla Firefox" >/dev/null; do
+        wmctrl -c "Mozilla Firefox"
+        sleep 0.5
+done
+
