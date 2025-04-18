761828a6324b21b6ecc6b6aad45f9e98084f763f
kongoro20 <kongo6411@gmail.com>
2025-04-18 12:59:20 +0200

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -23,11 +23,6 @@ for i in {1..500}; do
     echo "Running refresh.sh - Attempt $i" >> /root/restart_out.log
     bash refresh.sh
     sleep 890  # 30 minutes
-    # Check if the script exited with an error
-    if [ $? -ne 0 ]; then
-        echo "refresh.sh failed at attempt $i" | tee -a /root/restart_err.log
-        exit 1  # Exit if refresh.sh fails
-    fi
 done
 
 echo "restart.sh completed successfully after 500 iterations." >> /root/restart_out.log
